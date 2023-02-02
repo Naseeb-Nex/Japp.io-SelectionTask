8f1f9de02d1130c5856a7def294361130b36323c
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-02 14:44:25 +0530

    Feat: AppHeader UI added

--- lib/screens/Homesrc.dart
+++ lib/screens/Homesrc.dart
@@ -10,10 +10,66 @@ class Homesrc extends StatefulWidget {
 class _HomesrcState extends State<Homesrc> {
   @override
   Widget build(BuildContext context) {
+    // Screen Size
+    Size s = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-      ),
+      // body
+      body: SafeArea(
+          // Scroll view
+          child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        // main list
+        child: Column(children: [
+          // padding for the header
+          Padding(
+            padding: EdgeInsets.only(top: s.height * 0.02, bottom: s.height * 0.015),
+            // Appbar Section
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: s.width * 0.03),
+                  // back button
+                  child: Container(
+                    padding: const EdgeInsets.only(right: 10),
+                    height: 30,
+                    width: 30,
+                    child: Image.asset(
+                      "assets/icons/left-arrow.png",
+                    ),
+                  ),
+                ),
+                // tittle
+                const Text(
+                  "Statistics",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Nunito",
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                ),
+                // Downloads button
+                Padding(
+                  padding: EdgeInsets.only(right: s.width * 0.03),
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset(
+                      "assets/icons/list.png",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // body
+            // chart
+            // list
+        ]),
+      )),
     );
   }
 }
