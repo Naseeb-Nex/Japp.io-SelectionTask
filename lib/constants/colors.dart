89e6052d2195aecd1b0c1236f7438f38c751e0c3
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-02 19:17:58 +0530

    Updated with color schemes

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 
-const pcolor = Color(0XFFffffff); // Primary color
-const scolor = Color(0XFF558683); // Secondary color
+const pcolor = Color(0XFF558683); // Primary color
+const scolor = Color(0XFFffffff); // Secondary color
+const white = Colors.white; // White color
+const black = Colors.black; // Black color
+const gray = Color(0XFF929292); // Gray color
