d4822374828e9841bfeb13cbd29f18ad410f224d
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-02 19:21:27 +0530

    Feat: Date type selector adder
    Implemented tap fuctionality

--- lib/screens/Homesrc.dart
+++ lib/screens/Homesrc.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:japp_io_st/constants/colors.dart';
 
 class Homesrc extends StatefulWidget {
   const Homesrc({Key? key}) : super(key: key);
@@ -8,6 +9,8 @@ class Homesrc extends StatefulWidget {
 }
 
 class _HomesrcState extends State<Homesrc> {
+  String cal_type = "day"; // calendar type
+
   @override
   Widget build(BuildContext context) {
     // Screen Size
@@ -24,7 +27,8 @@ class _HomesrcState extends State<Homesrc> {
         child: Column(children: [
           // padding for the header
           Padding(
-            padding: EdgeInsets.only(top: s.height * 0.02, bottom: s.height * 0.015),
+            padding:
+                EdgeInsets.only(top: s.height * 0.02, bottom: s.height * 0.04),
             // Appbar Section
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -48,7 +52,7 @@ class _HomesrcState extends State<Homesrc> {
                   style: TextStyle(
                       color: Colors.black,
                       fontFamily: "Nunito",
-                      fontWeight: FontWeight.w500,
+                      fontWeight: FontWeight.w700,
                       fontSize: 20),
                 ),
                 // Downloads button
@@ -66,8 +70,132 @@ class _HomesrcState extends State<Homesrc> {
             ),
           ),
           // body
-            // chart
-            // list
+          // calander type selection row
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: s.width * 0.03),
+            child: Row(
+              children: [
+
+                // Day selection
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: InkWell(
+                    onTap: ()=> setState(() {
+                      cal_type = "day";
+                    }),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: s.height * 0.015),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: cal_type == "day" ? pcolor : white,
+                          ),
+                      child: Center(
+                        child: Text(
+                          "Day",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: cal_type == "day" ? white : gray,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Nunito",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // week selection
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: InkWell(
+                    onTap: ()=> setState(() {
+                      cal_type = "week";
+                    }),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: s.height * 0.015),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: cal_type == "week" ? pcolor : white,
+                          ),
+                      child: Center(
+                        child: Text(
+                          "Week",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: cal_type == "week" ? white : gray,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Nunito",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // month
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: InkWell(
+                    onTap: ()=> setState(() {
+                      cal_type = "month";
+                    }),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: s.height * 0.015),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: cal_type == "month" ? pcolor : white,
+                          ),
+                      child: Center(
+                        child: Text(
+                          "Month",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: cal_type == "month" ? white : gray,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Nunito",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // year selection
+                Flexible(
+                  flex: 1,
+                  fit: FlexFit.tight,
+                  child: InkWell(
+                    onTap: ()=> setState(() {
+                      cal_type = "year";
+                    }),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: s.height * 0.015),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: cal_type == "year" ? pcolor : white,
+                          ),
+                      child: Center(
+                        child: Text(
+                          "Year",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: cal_type == "year" ? white : gray,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Nunito",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+          // chart
+          // list
         ]),
       )),
     );
