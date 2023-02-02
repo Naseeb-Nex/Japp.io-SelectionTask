3d694d28aadb98d7700bdcdb8a12d2bcf8416bf7
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-02 11:33:55 +0530

    Updated: color scheme

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const pcolor = Color(0XFF558683); // Primary color
-const scolor = Color(0XFFffffff); // Secondary color
+const pcolor = Color(0XFFffffff); // Primary color
+const scolor = Color(0XFF558683); // Secondary color
