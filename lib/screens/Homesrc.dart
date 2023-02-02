a87e923d649896174b86a513682f4232b790107a
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-02 12:50:43 +0530

    refactor

---
+++ lib/screens/Homesrc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homesrc extends StatefulWidget {
+  const Homesrc({Key? key}) : super(key: key);
+
+  @override
+  State<Homesrc> createState() => _HomesrcState();
+}
+
+class _HomesrcState extends State<Homesrc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+      ),
+    );
+  }
+}
