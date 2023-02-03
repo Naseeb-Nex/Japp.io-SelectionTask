5b357df39afa5fa9edddbaae9253723bf99bd72a
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-03 12:50:57 +0530

    New Color set is added

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,5 @@ const scolor = Color(0XFFffffff); // Secondary color
 const white = Colors.white; // White color
 const black = Colors.black; // Black color
 const gray = Color(0XFF929292); // Gray color
+const tooltipbg = Color(0XFFF2F7F7);
+const tooltipborder = Color(0XFFB4CBC9);
